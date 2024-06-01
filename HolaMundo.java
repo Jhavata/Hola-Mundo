ec9169d8342b5c7c86de9b42ab42bd468eb95d90
Jose Gómez Vásquez <123199516+Jhavata@users.noreply.github.com>
2024-06-01 14:59:30 -0600

    pasar de Rama A a Rama Develop

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Esta rama es la A y voy para la rama develop");
+
     }
 }
